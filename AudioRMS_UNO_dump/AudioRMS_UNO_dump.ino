7e08ac35cd3e9b080ca39df384d881200be10d8b
KaipaUday <117348546+KaipaUday@users.noreply.github.com>
2023-07-03 18:02:59 +0200

    Version 1.0

--- AudioRMS_UNO_dump/AudioRMS_UNO_dump.ino
+++ AudioRMS_UNO_dump/AudioRMS_UNO_dump.ino
@@ -1,9 +1,7 @@
 
 #include <ShiftRegister74HC595.h>
-// #define NUM_BANDS 2
 #define numRegisters 10
-// Constants for filter coefficients and buffer size
-#define FILTER_ORDER 2  // Order of the filter (number of taps)
+#define FILTER_ORDER 2  // Order of the filter 
 #define BUFFER_SIZE (FILTER_ORDER + 1)   // Buffer size = filter order + 1, Buffer size, more buffersize delyed dying down
 // Buffer to store previous input samples
 float inputBuffer[BUFFER_SIZE] = {0};
@@ -23,22 +21,10 @@ int sensorPin = A0;        //pin number to use the ADC
 int sensorValue = 0;      //initialization of sensor variable, equivalent to EMA Y
 double samples[numSamples];
 double highPassedVal;
-// double inputSample[numSamples];
-//Global Variables
-// float inputBuffer[BUFFER_SIZE] = {0};
-// float EMA_a= 0.1;    //initialization of EMA alpha
-// int EMA_S= 0;        //initialization of EMA S
-// int highpass = 0;
-// int SpreadBinary[2];
-// const int rmsSample = 8;
-
-// int bandValues[] = {0,0};
-// arduinoFFT FFT = arduinoFFT(samples, isamples, numSamples, 8900);
 ShiftRegister74HC595<numRegisters> shiftRegister(serialData, shiftClock, latchClock);
 
 void setup()
 { // runs once at startup
-  // set pins to output so you can control the shift register
   pinMode(clearPin, OUTPUT);
   pinMode(shiftClock, OUTPUT);
   pinMode(latchClock, OUTPUT);
@@ -46,8 +32,6 @@ void setup()
   Serial.begin(115200);
   digitalWrite(clearPin, LOW);  // Pin is active-low, this clears the shift register
   digitalWrite(clearPin, HIGH); // Clear pin is inactive
-  
-  // EMA_S = analogRead(sensorPin);      //set EMA S for t=1
 }
 
 
@@ -87,8 +71,7 @@ void loop(){
   Serial.println(rms);
   int val= round(log10(rms)/log10(2)); //log2(x)= log10(x)/log10(2)
 
-
-  // Light up the LED bar based on the scaled value
+  // Light up the LED bar based on the scaled value- 
   for (int i = 0; i < numRegisters; i++) {
     if (i < val) {
       shiftRegister.set(i, HIGH);  // Turn on the segment
@@ -98,100 +81,3 @@ void loop(){
   }
 
 }
-
-// void showRMS(int rms, int rms2)
-// {
-//   digitalWrite(latchClock, LOW);                    // take the latchClock low so the LEDs don't change while you're sending in bits:
-//   shiftOut(serialData, shiftClock, MSBFIRST, rms2); // shift out the bits
-//   shiftOut(serialData, shiftClock, MSBFIRST, rms);  // shift out the bits
-//   digitalWrite(latchClock, HIGH);                   // take the latch pin high so the LEDs will light up
-
-// }
-// void spreadValue(float value, int SpreadBinary[])
-// {
-
-//   // Map the value from the input range to the spread range
-//   // float mappedValue = ((float)(value - minRange) / (float)(maxRange - minRange)) * (float)(spreadMax - spreadMin);
-//   // int spreadValue = int(mappedValue);
-//   int spreadValue= int(value);
-
-//   // to have lower continous incremental instead of discrete.
-//   if (spreadValue > 511)
-//   {
-//     SpreadBinary[0] = 255;
-//     SpreadBinary[1] = 3;
-//     return;
-//   }
-//   else if (spreadValue > 255)
-//   {
-//     SpreadBinary[0] = 255;
-//     SpreadBinary[1] = 1;
-//     return;
-//   }
-//   else if (spreadValue > 127)
-//   {
-//     SpreadBinary[0] = 255;
-//     SpreadBinary[1] = 0;
-//     return;
-
-//   }
-//   else if (spreadValue > 63)
-//   {
-//     SpreadBinary[0] = 127;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else if (spreadValue > 31)
-//   {
-//     SpreadBinary[0] = 63;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else if (spreadValue > 15)
-//   {
-//     SpreadBinary[0] = 31;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else if (spreadValue > 7)
-//   {
-//     SpreadBinary[0] = 15;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else if (spreadValue > 3)
-//   {
-//     SpreadBinary[0] = 7;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else if (spreadValue > 1)
-//   {
-//     SpreadBinary[0] = 3;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else if (spreadValue > 0)
-//   {
-//     SpreadBinary[0] = 1;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-//   else
-//   {
-//     SpreadBinary[0] = 0;
-//     SpreadBinary[1] = 0;
-//   return;
-
-//   }
-
-//   // Serial.println(SpreadBinary[0]+SpreadBinary[1]);
-// }
-
